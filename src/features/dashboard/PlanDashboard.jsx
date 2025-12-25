3bda5fb827aae7c94b44c58e6353fd447f6c7e46
no-kris <kris.tresk@gmail.com>
2025-12-25 16:33:27 -0500

    Added a check in button in the plan dashboard. This will allow the user to input feedback for the next week. The button is disabled until all the workouts in the current week are complete.

--- src/features/dashboard/PlanDashboard.jsx
+++ src/features/dashboard/PlanDashboard.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import Button from "../../components/ui/Button";
 import Schedule from "./Schedule";
-import { Notebook } from "lucide-react";
+import { CheckCircle2, Notebook, PlusCircle } from "lucide-react";
 
 export default function PlanDashboard({ plan, history, setView }) {
   const [weekIndex, setWeekIndex] = useState(0);
   const [details, setDetails] = useState(null);
   const [activeWorkout, setActiveWorkout] = useState(null);
+  const [showCheckin, setShowCheckin] = useState(false);
 
   if (!plan || !plan.weeks || plan.totalWeeks === 0)
     return (
@@ -26,7 +27,7 @@ export default function PlanDashboard({ plan, history, setView }) {
     );
 
   const handleCompleted = (day) => {
-    history.some(
+    return history.some(
       (h) =>
         h.workout.dayName === day.dayName &&
         h.workout.weekNumber === currentWeek.weekNumber
@@ -37,6 +38,17 @@ export default function PlanDashboard({ plan, history, setView }) {
     setActiveWorkout(day);
     setView("active");
   };
+
+  const isLastAvailableWeek = weekIndex === plan.weeks.length - 1;
+  const isProgramCompleted = currentWeek.weekNumber >= plan.totalWeeks;
+
+  const allWorkoutsCompleted = currentWeek.schedule.every((day) =>
+    history.some(
+      (h) =>
+        h.workout.dayName === day.dayName &&
+        h.workout.weekNumber === currentWeek.weekNumber
+    )
+  );
 
   return (
     <div className="view-container p-2">
@@ -89,6 +101,27 @@ export default function PlanDashboard({ plan, history, setView }) {
           onStartWorkout={handleStartWorkout}
         />
       </div>
+
+      {isProgramCompleted ? (
+        <div className="card flex text-center border-primary">
+          <CheckCircle2 size={40} className="text-primary mx-auto mb-2" />
+          <h3 className="text-primary font-bold">PROGRAM COMPLETE</h3>
+          <p className="text-muted text-xs mt-2">
+            SYSTEM STANDBY. RESET TO BEGIN NEW CYCLE.
+          </p>
+        </div>
+      ) : (
+        isLastAvailableWeek && (
+          <Button
+            text={`CHECK-IN & GENERATE WEEK ${currentWeek.weekNumber + 1}`}
+            Icon={PlusCircle}
+            iconSize={24}
+            onClick={() => setShowCheckin(true)}
+            className="flex items-center justify-center gap-4 button btn-primary w-full font-bold text-xl uppercase p-4 letter-spacing-2 mt-4"
+            disabled={!allWorkoutsCompleted}
+          />
+        )
+      )}
     </div>
   );
 }
