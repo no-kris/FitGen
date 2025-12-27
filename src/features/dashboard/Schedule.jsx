8c279130d61d23c065eeeea5768f920eb4411571
no-kris <kris.tresk@gmail.com>
2025-12-27 18:05:41 -0500

    Fixed a bug in Schedule.jsx that wasn't showing completed workouts.

--- src/features/dashboard/Schedule.jsx
+++ src/features/dashboard/Schedule.jsx
@@ -39,7 +39,7 @@ export default function Schedule({
                   onClick={() =>
                     onStartWorkout({
                       ...day,
-                      weekNum: currentWeek.weekNumber,
+                      weekNumber: currentWeek.weekNumber,
                     })
                   }
                   Icon={Play}
